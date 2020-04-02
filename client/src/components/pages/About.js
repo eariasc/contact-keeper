3843764ab7d4f1e45eae5386c63aa4d263d342fd
Esteban Arias <eariasc@Estebans-MacBook-Pro.local>
2020-04-02 14:31:12 -0600

    Prepare contact-keeper app for deploy

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About this app</h1>
+            <p className="my-1">
+                This is a fullstack React app for keeping contract
+            </p>
+            <p className="bg bg-dark p">
+                <strong>Version: 1.0.0</strong>
+            </p>
+        </div>
+                                                                                                                                                                                     
+    );
+}
+
+export default About;
--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => 
-    res.json({msg: 'Welcome to the contactKeeper API'})
-);
+//Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({extended: false}));
 
 //Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'productiom') {
+    // Set a static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server starter on port: ${PORT}`);
